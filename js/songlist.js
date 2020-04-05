0afcb15d547c51ab8e327b7811d5cfc6c426f619
Rafał Hejna <rafal.hejna@gmail.com>
2020-04-05 02:30:34 +0200

    Local version + js refactor

---
+++ js/songlist.js
@@ -0,0 +1,75 @@
+window.page.songList = [
+    {
+        id: '-wTDsCplARY',
+        name: 'Furret Walk with Lyrics',
+        start: 0,
+        end: 104
+    },
+    {
+        id: 'VMycOhwjhQg',
+        name: '10 Levels of Spooky Scary Skeletons',
+        start: 36,
+        end: 172
+    },
+    {
+        id: 'xzD0M1MXGqY',
+        name: 'Furret Walk with Fan Lyrics',
+        start: 0
+    },
+    {
+        id: 'E-MaslEgG9o',
+        name: 'Furretalovania',
+        start: 0,
+        end: 131
+    },
+    {
+        id: 'jEK89ZhTVes',
+        name: 'Hit on run',
+        start: 0
+    },
+    {
+        id: 'P9-SUS1j1vE',
+        name: 'Just wait a moment...',
+        start: 0
+    },
+    {
+        id: 'iF2dhmiEMIc',
+        name: 'Hit on run Drum Cover',
+        start: 0
+    },
+    {
+        id: 'kIP8kkqp5gY',
+        name: 'Suzumu - Kasyokusei: Idol Syoukougun',
+        start: 2
+    },
+    {
+        id: 'ulxoIly3jt4',
+        name: '0 strings bass solo',
+        start: 0,
+        end: 144
+    },
+    {
+        id: 'cytyAgu9-bA',
+        name: 'Impossible Deadmaus bass line',
+        start: 18,
+        end: 135
+    },
+    {
+        id: '97KLphniRAA',
+        name: 'Slayer - Bloodline ukulele cover',
+        start: 0,
+        end: 202
+    },
+    {
+        id: 'KEodtWN7Xb0',
+        name: 'Slayer - Payback ukulele cover',
+        start: 0,
+        end: 222
+    },
+    {
+        id: 'cRdlY_usbiA',
+        name: 'Slayer - Piece by piece ukulele cover',
+        start: 0,
+        end: 190
+    }
+];
